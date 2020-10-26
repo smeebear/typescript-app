02db4816fd4966b45129b2d27caf56d1c4c56f9a
SmeeBear <soheron@gmail.com>
2020-10-26 18:33:48 -0500

    Added question card component, grabbed background image, created some stuff, I forgot to commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 // import logo from './logo.svg';
 // import './App.css';
+import QuestionCard from './components/QuestionCard';
 
 function App() {
+
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const startTrivia = async () => {
+
+  }
+
+  const checkAns = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
     <div className="App">
-      Quiz
+      <h1>React Quiz</h1>
+      <button className="start" onClick={startTrivia}>Start</button>
+      <p className="score">Score: </p>
+      <p>Loading Questions...</p>
+      <QuestionCard />
+      <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
 }
