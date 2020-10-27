7746e3cd83df05a9ea4750b3e3a3b077b73ae3e6
smeebear <soheron@gmail.com>
2020-10-27 00:11:24 -0500

    Styled start and next buttons

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -39,7 +39,23 @@ export const Wrapper = styled.div`
     h1 {
         filter: drop-shadow(2px 2px #fff);
         font-size: 70px;
+        font-weight: 100;
         text-align: center;
         margin: 20px;
+    }
+
+    .start, .next {
+        cursor: pointer;
+        background: linear-gradient(180deg, #fff, #ccc);
+        border: 2px solid #bbb;
+        box-shadow: 0px 5px 10px rgba(0,0,0,0.25);
+        border-radius: 10px;
+        height: 40px;
+        margin: 20px 0;
+        padding: 0 40px;
+    }
+
+    .start {
+        max-width: 200px;
     }
 `
